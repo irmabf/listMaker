a7b9184d38073ce4c64126445c55a0989e2143dd
Irma <irblancofdz@gmail.com>
2018-06-27 17:16:27 +0200

    Set up basic app skeleton

---
+++ app/src/main/java/com/irmabf/listmaker/ListSelectionRecyclerViewAdapter.kt
@@ -0,0 +1,18 @@
+package com.irmabf.listmaker
+
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+
+class ListSelectionRecyclerViewAdapter: RecyclerView.Adapter<ListSelectionViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListSelectionViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: ListSelectionViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
---
+++ app/src/main/java/com/irmabf/listmaker/ListSelectionViewHolder.kt
@@ -0,0 +1,7 @@
+package com.irmabf.listmaker
+
+import android.support.v7.widget.RecyclerView
+import  android.view.View
+
+class ListSelectionViewHolder(itemView: View?): RecyclerView.ViewHolder(itemView) {
+}
